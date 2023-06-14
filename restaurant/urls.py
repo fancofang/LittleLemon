6efd85eb92ade54618bea1f1c437c0f4718cccc8
fanco <fanghao23@hotmail.com>
2023-06-14 14:22:47 +0100

    d<!--add this template tag-->

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.index, name="index"),
+    # path('', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('book/', views.book, name="book"),
     path('reservations/', views.reservations, name="reservations"),
     path('menu/', views.menu, name="menu"),
     path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
-    path('bookings', views.bookings, name='bookings'), 
+    path('bookings', views.bookings, name='bookings'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -11,8 +11,11 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html',{})
+
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index_backup.html')
 
 def about(request):
     return render(request, 'about.html')
